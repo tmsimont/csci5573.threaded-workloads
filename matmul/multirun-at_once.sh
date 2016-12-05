ff0ae37837b861fcb1798f680c3f6ee14909f6d5
tmsimont <tmsimont@gmail.com>
2016-12-05 16:48:37 +0000

    move at once sh file to old

--- matmul/multirun-at_once.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run the given problem x amount of times, launching all instances at once
-# usage: ./multirun-at_once [num of processes to run] [outfile]
-for i in `seq 1 $1`
-do
-	#chrt -r 99 ./matmul.out -l1 -n6 -o0 -m1 -p1 >> $2 &
-	./matmul.out -l1 -n6 -o0 -m1 -p1 >> $2 &
-done
